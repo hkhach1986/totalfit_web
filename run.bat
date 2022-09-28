6bfdc8c6726aa3e85ddc745ca53fdb0c24979b93
hkhach1986 <74899626+hkhach1986@users.noreply.github.com>
2022-09-28 21:43:40 +0400

    Update run.bat

--- run.bat
+++ run.bat
@@ -1,5 +1,5 @@
-pytest -v -s  -m "sanity" --browser firefox --html=Reports\report_sanity.html  --capture=tee-sys .\testcases\
+rem pytest -v -s  -m "sanity" --browser firefox --html=Reports\report_sanity.html  --capture=tee-sys .\testcases\
 rem pytest -v -s  -m "sanity" --browser chrome --html=Reports\report_sanity.html  --capture=tee-sys .\testcases\
-REM pytest -v -s  -m "regression" --browser firefox --html=Reports\report_sanity.html  --capture=tee-sys .\testcases\
+pytest -v -s  -m "regression" --browser firefox --html=Reports\report_sanity.html  --capture=tee-sys .\testcases\
 REM pytest -v -s  -m "sanity and regression" --browser firefox --html=Reports\report_sanity.html  --capture=tee-sys .\testcases\
 REM pytest -v -s  -m "sanity or regression" --browser firefox --html=Reports\report_sanity.html  --capture=tee-sys .\testcases\
